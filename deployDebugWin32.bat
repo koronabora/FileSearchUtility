8e7df67f0f38cb098f278e5f46fb915c41db42be
koronabora <koronabora@gmail.com>
2020-04-30 11:02:55 +0300

    +

--- deployDebugWin32.bat
+++ deployDebugWin32.bat
@@ -1,3 +1,5 @@
 @echo off
 cd Debug
 d:\Qt\5.12.6\msvc2017\bin\windeployqt.exe --debug --compiler-runtime FileSearchUtility.exe
+xcopy .\FileSearchUtility\FileSearchUtility_en.ts .\Debug /y
+xcopy .\FileSearchUtility\FileSearchUtility_ru.ts .\Debug /y
--- deployReleaseWin32.bat
+++ deployReleaseWin32.bat
@@ -1,3 +1,5 @@
 @echo off
 cd Release
 d:\Qt\5.12.6\msvc2017\bin\windeployqt.exe --release --compiler-runtime FileSearchUtility.exe
+xcopy .\FileSearchUtility\FileSearchUtility_en.ts .\Release /y
+xcopy .\FileSearchUtility\FileSearchUtility_ru.ts .\Release /y
